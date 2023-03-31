94e42bf781b8d3a05e933704bc2bd786c9008725
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-04-01 03:00:27 +0700

    Реализован функционал открытия/закрытия модальных окон и табов переключения контента

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,36 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => item.style.display = 'none');
+
+        tab.forEach(item => item.classList.remove(activeClass));
+    }
+
+    function showTabContent(n = 0) {
+        content[n].style.display = 'block';
+
+        tab[n].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const currElement = e.target;
+        const _tabClass = tabSelector.replace(/\./, '');
+
+        if (currElement && (currElement.classList.contains(_tabClass) || currElement.parentNode.classList.contains(_tabClass))) {
+            tab.forEach((item, i) => {
+                if (currElement === item || currElement.parentNode === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
+}
+
+export default tabs;
