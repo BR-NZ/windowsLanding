9b1b018c60f9ceac2d9503a0bd0d29fec681aaef
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-04-06 17:13:11 +0700

    Добавлены анимации для контента табов, исправлено дергание попап-окон при исчезновении скролл-бара

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,6 +3,7 @@ const modals = (state) => {
         const trigger = document.querySelectorAll(triggerSelector);
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
+        const scrollWidth = calcScroll();
         
         const incompleteMessage = document.createElement('p');
         incompleteMessage.classList.add('status');
@@ -27,6 +28,7 @@ const modals = (state) => {
                     closeModals('[data-modal]');
                     modal.style.display = 'block';
                     document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = scrollWidth + 'px';
                 } else {
                     item.insertAdjacentElement('afterend', incompleteMessage);
                 }
@@ -38,6 +40,7 @@ const modals = (state) => {
 
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = '';
         })
 
         modal.addEventListener('click', (e) => {
@@ -46,6 +49,7 @@ const modals = (state) => {
 
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
+                document.body.style.marginRight = '';
             }
         });
     }
@@ -57,13 +61,24 @@ const modals = (state) => {
         }, time)
     }
 
+    function calcScroll() {
+        const scrollBox = document.createElement('div');
+        scrollBox.style.cssText = 'width: 10px; height: 10px; overflow-y: scroll; visibility: hidden;';
+        document.body.append(scrollBox);
+
+        const scrollWidth = scrollBox.offsetWidth - scrollBox.clientWidth;
+        scrollBox.remove();
+
+        return scrollWidth;
+    }
+
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 
-    // showModalByTime('.popup', 60000);
+    showModalByTime('.popup', 60000);
 }
 
 function closeModals(selector) {
